c341285e0fc4ee1152a00fa8673ec9101c7759db
SixOrca <sixorca00@gmail.com>
2026-06-19 11:55:53 -0500

    first commit

---
+++ src/main/java/com/crudzaso/dbmanager/juanchito/JuanchitoApplication.java
@@ -0,0 +1,13 @@
+package com.crudzaso.dbmanager.juanchito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuanchitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuanchitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudzaso/dbmanager/juanchito/JuanchitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudzaso.dbmanager.juanchito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuanchitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
